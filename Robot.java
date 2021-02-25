529bfeb68c35adf8a6d69bf628b5c56045d763f3
jbb1209 <65250598+jbb1209@users.noreply.github.com>
2021-02-25 14:15:01 -0500

    Add files via upload

--- Robot.java
+++ Robot.java
@@ -1,4 +1,24 @@
 
 public class Robot {
+	public Robot (String name)
+	{
+		
+	}
+	public void pickup()
+	{
+		System.out.print("Oh treasure, picking it up!");
+	}
+	public void Jump()
+	{
+		System.out.print("Oh no a hazard, I'm jumping over it.");
+	}
+	public void Fire()
+	{
+		System.out.print("Bad guys! Fire my blow torch at them.");
+	}
+	public void Heal()
+	{
+		System.out.print("Thanks for healing my wounds.");
+	}
 
 }
